71b95767aa4fa042cd53a59a1b79e8ea5f925679
arseniy <aoart2006@yandex.ru>
2022-05-07 22:53:03 +0300

    2 problem

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 import sys
-from io import BytesIO
 
 import requests
-from PIL import Image
 from PyQt5 import uic
 from PyQt5.Qt import *
-
 
 
 class Main(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('gg.ui', self)
-        self.initUI()
-
-    
-    def initUI(self):
+        self.coord = ''
+        self.zoom1 = 0.0
+        self.zoom2 = 0.0
+        self.flag = True
+        self.map_request = ''
+        self.response = ''
         self.pushButton.clicked.connect(self.get_picture)
+        self.map_file = "map.png"
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Up and self.zoom1 < 25.0 and self.zoom2 < 25.0:
+            self.zoom1 += 2
+            self.zoom2 += 2
+            self.get_picture()
+
+        if event.key() == Qt.Key_Down and self.zoom1 > 2.0 and self.zoom2 > 2.0:
+            self.zoom1 -= 2
+            self.zoom2 -= 2
+            self.get_picture()
 
     def get_picture(self):
-        self.map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.lineEdit.text()}&spn={self.lineEdit_4.text()}&l=sat"
+        if self.flag:
+            self.coord = self.lineEdit.text()
+            self.zoom1 = float(self.lineEdit_4.text().split(',')[0])
+            self.zoom2 = float(self.lineEdit_4.text().split(',')[1])
+            self.flag = False
+        self.map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.coord}&spn={self.zoom1},{self.zoom2}&l=sat"
         self.response = requests.get(self.map_request)
-        self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(self.response.content)
         self.MAP.setPixmap(QPixmap(self.map_file))
